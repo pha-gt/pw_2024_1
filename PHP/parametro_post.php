d83b81f62156044a845f896bb342dccc9cff29ee
Fabian Gonzalez <fgtfabian@gmail.com>
2024-02-17 21:47:21 -0600

    Actualizacion con Bootstrap

--- PHP/parametro_post.php
+++ PHP/parametro_post.php
@@ -2,6 +2,17 @@
 $nombre_atr=isset($_POST["nombre_atr"])?$_POST["nombre_atr"]:"NO HAY DATOS";
 ?>
 
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Datos capturados</h1>
 Hola <?php echo $nombre_atr;?>
+</body>
+</html>
